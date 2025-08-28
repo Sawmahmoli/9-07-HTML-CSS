04c36aefaa95ce895b4258e8be348424d57460af
Sawmahmoli <sswmhh2022@gmail.com>
2025-08-28 17:06:38 +0800

    0828-1

--- 01 花室/03 web/js/script.js
+++ 01 花室/03 web/js/script.js
@@ -1,5 +1,13 @@
 $(function () {
 
+    // 漢堡按鈕
+    $('.hamburger').click(function () {
+        // 切換 ≡ => X
+        $(this).toggleClass('is-active');
+        $('.navigation').toggleClass('show');
+
+    });
+
     // 捲動至指定位置
     $(".menu a").click(function () {
         var btn = $(this).attr("href");
@@ -9,17 +17,23 @@ $(function () {
 
     // gotop
     $("#gotop").click(function () {
-        $("html,body").animate({ scrollTop: pos.top }, 1000);
+        $("html,body").animate({ scrollTop: 0 }, 1000);
     });
 
     // 淡出淡入
     $(window).scroll(function () {
-        if ( $(this).scrollTop() > 200 ) {
-            $("gotop").stop().fadeIn("fast",1);
+        if ($(this).scrollTop() > 200) {
+            $("gotop").stop().fadeIn("fast", 1);
         } else {
             $("gotop").stop().fadeOut("fast");
         }
     });
 
 });
+
+// 共用預設
+$('.smoove').smoove(
+    { offset: 200 }
+);
+
 
