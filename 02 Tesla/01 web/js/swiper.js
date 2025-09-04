a9d434e347c02cdc95536abbba0d53b72be1c9b5
Sawmahmoli <sswmhh2022@gmail.com>
2025-09-04 16:56:31 +0800

    0904-1

--- 02 Tesla/01 web/js/swiper.js
+++ 02 Tesla/01 web/js/swiper.js
@@ -1,9 +1,15 @@
+// 換圖輪播 JavaScript
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 1.25,
-      spaceBetween: 30,
-      freeMode: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+  slidesPerView: 1.25,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// jQuery UI tabs => 套件衝突！改為在 HTML 中置入
+//  $( function() {
+//     $( "#album" ).tabs();
+//   } );
